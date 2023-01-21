549d866b91ff0bdb8c8973e42ea580c4674f0e63
Debtanu Maitra <maitradebtanu@gmail.com>
2023-01-21 18:49:11 +0530

    Solution_4

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -22,26 +22,26 @@ public class F1 extends Car {
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
-            changeGear(1);
+            this.changeGear(1);
         }
         //for all other cases, change the gear accordingly
-        if(newSpeed >= 1 && newSpeed <=50) {
-            changeGear(1);
+        else if(newSpeed >= 1 && newSpeed <=50) {
+            this.changeGear(1);
         }
-        if(newSpeed >= 51 && newSpeed <= 100) {
-            changeGear(2);
+        else if(newSpeed >= 51 && newSpeed <= 100) {
+            this.changeGear(2);
         }
-        if(newSpeed >= 101 && newSpeed <= 150) {
-            changeGear(3);
+        else if(newSpeed >= 101 && newSpeed <= 150) {
+            this.changeGear(3);
         }
-        if(newSpeed >= 151 && newSpeed <= 200) {
-            changeGear(4);
+        else if(newSpeed >= 151 && newSpeed <= 200) {
+            this.changeGear(4);
         }
-        if(newSpeed >= 201 && newSpeed <= 250) {
-            changeGear(5);
+        else if(newSpeed >= 201 && newSpeed <= 250) {
+            this.changeGear(5);
         }
-        if(newSpeed > 250) {
-            changeGear(6);
+        else if(newSpeed > 250) {
+            this.changeGear(6);
         }
 
         if(newSpeed > 0) {
